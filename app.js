9a6cb0de5df910dccc6809aa8db2419f783f752d
ashish082003 <aj9172243856@gmail.com>
2023-08-18 16:42:14 +0530

    Level 2 Authentication

---
+++ app.js
@@ -0,0 +1,67 @@
+const express=require('express');
+const bodyParser=require('body-parser');
+const ejs=require('ejs');
+const mongoose =require('mongoose');
+const encrypt=require("mongoose-encryption");
+
+const app=express();
+app.use(express.static("public"));
+app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+
+mongoose.connect("mongodb://127.0.0.1:27017/userDB",{useNewUrlParser:true});
+
+const userSchema=new mongoose.Schema({
+    email:String,
+    password:String
+});
+const secret="ThisisourLitteSecrect.";
+userSchema.plugin(encrypt,{secret:secret,encryptedFields:["password"]});
+const User=new mongoose.model("User",userSchema);
+
+
+app.get("/",(req,res)=>{
+    res.render("home");
+});
+
+app.get('/login',(req,res)=>{
+    res.render("login");
+});
+
+app.get('/register',(req,res)=>{
+    res.render("register");
+});
+
+app.post("/register",(req,res)=>{
+    const newUser=new User({
+        email:req.body.username,
+        password:req.body.password
+    });
+    newUser.save().then(function(){
+        res.render("secrets");
+    }).catch(function(err){
+        console.log(err);
+    })
+})
+
+app.post("/login",(req,res)=>{
+    const username=req.body.username;
+    const password=req.body.password;
+
+    User.findOne({email:username}).then(function(founduser){
+        if(founduser.password===password){
+            res.render("secrets");
+        }else{
+            console.log("Password is incorrect");
+        }
+    }).catch(function(err){
+        console.log(err);
+    })
+})
+// app.get("/:logout",(req,res)=>{
+//     res.render("home");
+// })
+app.listen(3000,()=>{
+    console.log("server is listed on port 3000");
+});
+
